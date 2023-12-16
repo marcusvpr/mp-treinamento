13929e4588a32ddfd3fb771ced356138cc64073b
MarcusVPR <marcus_vpr@hotmail.com>
2023-12-16 13:36:18 -0300

    feat: Config JSF Primefaces + JPA + Tela Flight ...

--- apps-v0/src/main/java/com/mpxds/apps/IniciandoApplication.java
+++ apps-v0/src/main/java/com/mpxds/apps/IniciandoApplication.java
@@ -1,13 +1,34 @@
 package com.mpxds.apps;
 
+import javax.faces.webapp.FacesServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import javax.servlet.ServletContext;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class IniciandoApplication {
-
+	//
 	public static void main(String[] args) {
+		//
 		SpringApplication.run(IniciandoApplication.class, args);
 	}
+
+    @Bean
+    ServletRegistrationBean jsfServletRegistration (ServletContext servletContext) {
+        //spring boot only works if this is set
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", 
+                                                            Boolean.TRUE.toString());
+        //registration
+        ServletRegistrationBean srb = new ServletRegistrationBean();
+
+        srb.setServlet(new FacesServlet());
+        srb.setUrlMappings(Arrays.asList("*.xhtml"));
+        srb.setLoadOnStartup(1);
+        //
+        return srb;
+    }
 
 }
