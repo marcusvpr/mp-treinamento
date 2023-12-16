f7365afdb3313d95f7c0adf08e523dfb8bda0eb9
MarcusVPR <marcus_vpr@hotmail.com>
2023-12-16 10:53:58 -0300

    feat: Initial Commit

---
+++ apps-v0/src/main/java/com/mpxds/apps/IniciandoApplication.java
@@ -0,0 +1,13 @@
+package com.mpxds.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniciandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IniciandoApplication.class, args);
+	}
+
+}
---
+++ apps-v0/src/test/java/com/mpxds/apps/IniciandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpxds.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniciandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
